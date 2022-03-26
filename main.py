5528066c9df6ed1421ddf3944cc9d4de05e8b92a
jbrown1618 <jbrown1618@github.com>
2022-03-26 19:06:57 -0400

    First attempt at spotify auth

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 def main():
-    
+    with open('/Users/jbrown1618/Desktop/spotify-client-id') as f:
+        client_id = f.read()
+
+    with open('/Users/jbrown1618/Desktop/spotify-client-secret') as f:
+        client_secret = f.read()
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, 
+                                                   client_secret=client_secret, 
+                                                   redirect_uri="http://localhost:3000/",
+                                                   open_browser=False,
+                                                   scope="user-library-read"))
+
+    results = sp.current_user_saved_tracks()
+
 if __name__ == '__main__':
     main()
