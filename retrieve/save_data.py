119b048c691705e80b955491e92a4e8ff6ccd2b7
jbrown1618 <jbrown1618@github.com>
2022-07-26 18:27:35 -0400

    Save more data

--- retrieve/save_data.py
+++ retrieve/save_data.py
@@ -5,6 +5,23 @@ import pandas as pd
 
 page_size = 50
 
+processed_playlists = set()
+processed_tracks = set()
+processed_artists = set()
+processed_albums = set()
+
+playlists_data = []
+tracks_data = []
+artists_data = []
+albums_data = []
+audio_features = []
+
+liked_tracks = []
+playlist_track = []
+track_artist = []
+album_artist = []
+album_track = []
+
 def save_data():
     if not os.path.isdir("./output"):
         os.mkdir("./output")
@@ -20,19 +37,50 @@ def save_data():
                                                    redirect_uri="http://localhost:3000/",
                                                    open_browser=False,
                                                    scope="user-library-read"))
-    save_tracks_data(sp)
+    save_playlists_data(sp)
+    save_liked_tracks_data(sp)
+    save_audio_features_data(sp)
+
+    pd.DataFrame(playlists_data).to_csv("./output/playlists.csv")
+    pd.DataFrame(tracks_data).to_csv("./output/tracks.csv")
+    pd.DataFrame(artists_data).to_csv("./output/artists.csv")
+    pd.DataFrame(albums_data).to_csv("./output/albums.csv")
+    pd.DataFrame(liked_tracks).to_csv("./output/liked_tracks.csv")
+    pd.DataFrame(playlist_track).to_csv("./output/playlist_track.csv")
+    pd.DataFrame(track_artist).to_csv("./output/track_artist.csv")
+    pd.DataFrame(album_artist).to_csv("./output/album_artist.csv")
+    pd.DataFrame(album_track).to_csv("./output/album_track.csv")
+    pd.DataFrame(audio_features).to_csv("./output/audio_features.csv")
 
 
-def save_tracks_data(sp: spotipy.Spotify):
-    artist_uris = set()
-    album_uris = set()
+def save_playlists_data(sp: spotipy.Spotify):
+    offset = 0
+    has_more = True
+    while has_more:
+        playlists = sp.current_user_playlists(limit=page_size, offset=offset)
+        for playlist in playlists["items"]:
+            process_playlist(playlist)
+            save_playlist_tracks_data(sp, playlist["uri"])
 
-    tracks_data = []
-    artists_data = []
-    albums_data = []
-    track_artist = []
-    album_artist = []
+        has_more = offset + page_size < playlists["total"]
+        offset += page_size
 
+
+def save_playlist_tracks_data(sp: spotipy.Spotify, playlist_uri):
+    offset = 0
+    has_more = True
+    while has_more:
+        tracks = sp.playlist_tracks(playlist_uri, limit=page_size, offset=offset)
+        for item in tracks["items"]:
+            track = item["track"]
+            playlist_track.append({ "playlist_uri": playlist_uri, "track_uri": track["uri"] })
+            process_track(track)
+
+        has_more = offset + page_size < tracks["total"]
+        offset += page_size
+        
+
+def save_liked_tracks_data(sp: spotipy.Spotify):
     offset = 0
     has_more = True
     while has_more:
@@ -40,38 +88,69 @@ def save_tracks_data(sp: spotipy.Spotify):
 
         for item in saved_tracks["items"]:
             track = item["track"]
-            tracks_data.append(track_data(track))
-
-            for artist in track["artists"]:
-                track_artist.append({ "track_uri": track["uri"], "artist_uri": artist["uri"] })
-
-                if (not artist["uri"] in artist_uris):
-                    artists_data.append(artist_data(artist))
-                    artist_uris.add(artist["uri"])
-
-            album = track["album"]
-            if (not album["uri"] in album_uris):
-                albums_data.append(album_data(album))
-                album_uris.add(album["uri"])
-
-                for artist in album["artists"]:
-                    album_artist.append({ "album_uri": album["uri"], "artist_uri": artist["uri"] })
-                    if (not artist["uri"] in artist_uris):
-                        artists_data.append(artist_data(artist))
-                        artist_uris.add(artist["uri"])
+            liked_tracks.append({ "track_uri": track["uri"] })
+            process_track(track)
 
         has_more = offset + page_size < saved_tracks["total"]
         offset += page_size
 
-    pd.DataFrame(tracks_data).to_csv("./output/tracks.csv")
-    pd.DataFrame(artists_data).to_csv("./output/artists.csv")
-    pd.DataFrame(albums_data).to_csv("./output/albums.csv")
-    pd.DataFrame(track_artist).to_csv("./output/track_artist.csv")
-    pd.DataFrame(album_artist).to_csv("./output/album_artist.csv")
+
+def save_audio_features_data(sp: spotipy.Spotify):
+    queue = [track_uri for track_uri in processed_tracks]
+    while len(queue) > 0:
+        next = queue[0:page_size]
+        queue = queue[page_size:]
+
+        features = sp.audio_features(tracks=next)
+        for track_features in features:
+            process_audio_features(track_features)
 
 
-def save_playlists_data(sp: spotipy.Spotify):
-    playlists = sp.current_user_playlists()
+def process_playlist(playlist):
+    if playlist["uri"] in processed_playlists:
+        return
+
+    playlists_data.append(playlist_data(playlist))
+
+
+def process_track(track):
+    if track["uri"] in processed_tracks:
+        return
+
+    tracks_data.append(track_data(track))
+    processed_tracks.add(track["uri"])
+    
+    for artist in track["artists"]:
+        track_artist.append({ "track_uri": track["uri"], "artist_uri": artist["uri"] })
+        process_artist(artist)
+    
+    album = track["album"]
+    album_track.append({ "album_uri": album["uri"], "track_uri": track["uri"] })
+    process_album(album)
+
+            
+def process_artist(artist):
+    if artist["uri"] in processed_artists:
+        return
+
+    artists_data.append(artist_data(artist))
+    processed_artists.add(artist["uri"])
+
+
+def process_album(album):
+    if album["uri"] in processed_albums:
+        return
+
+    albums_data.append(album_data(album))
+    processed_albums.add(album["uri"])
+
+    for artist in album["artists"]:
+        album_artist.append({ "album_uri": album["uri"], "artist_uri": artist["uri"] })
+        process_artist(artist)
+
+
+def process_audio_features(features):
+    audio_features.append(audio_features_data(features))
 
 
 def track_data(track):
@@ -88,23 +167,19 @@ def artist_data(artist):
 
 
 def album_data(album):
-    fields = ["album_type", "name", "release_date", "uri"]
+    fields = ["name", "album_type", "release_date", "uri"]
     data = {field: album[field] for field in fields}
     return data
 
 
+def playlist_data(playlist):
+    fields = ["name", "collaborative", "public", "uri"]
+    data = {field: playlist[field] for field in fields}
+    return data
 
 
-        # playlists = sp.current_user_playlists()
-    # print(playlists)
-
-    # audio_features = sp.audio_features(tracks=['spotify:track:0a0FISfY8ty1xC69xCWf2T'])
-    # print(audio_features)
-
-    # ranges = ['short_term', 'medium_term', 'long_term']
-    # for sp_range in ranges:
-    #     print("range:", sp_range)
-    #     results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
-    #     for i, item in enumerate(results['items']):
-    #         print(i, item['name'], '//', item['artists'][0]['name'])
-    #     print()
+def audio_features_data(features):
+    fields = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "time_signature"]
+    data = {field: features[field] for field in fields}
+    data["track_uri"] = features["uri"]
+    return data
