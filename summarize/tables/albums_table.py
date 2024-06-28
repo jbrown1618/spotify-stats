a8507ab67e04477d637d9565f1059203e6fe4049
Josh Brown <jbrown1618@github.com>
2024-06-28 15:44:55 -0400

    Add rank to albums table

--- summarize/tables/albums_table.py
+++ summarize/tables/albums_table.py
@@ -6,10 +6,10 @@ from utils.util import spotify_link, first
 def albums_table(tracks: pd.DataFrame):
     grouped = tracks.groupby("album_uri").agg({"track_uri": "count", "track_liked": "sum", "album_name": first, "album_image_url": first, "album_release_date": first}).reset_index()
     grouped = grouped.sort_values(by=["track_uri", "track_liked", "album_name"], ascending=False)
-    grouped = grouped.rename(columns={"track_uri": "Tracks", "track_liked": "💚", "album_name": "Album", "album_release_date": "Release Date"})
+    grouped = grouped.rename(columns={"track_uri": "Tracks", "track_liked": "💚", "album_name": "Album", "album_release_date": "Release Date", "album_rank": "Rank"})
 
     grouped["Art"] = grouped["album_image_url"].apply(lambda src: md_image("", src, 50))
     grouped["🔗"] = grouped["album_uri"].apply(lambda uri: spotify_link(uri))
-    table_data = grouped[["Art", "Tracks", "💚", "Album", "Release Date", "🔗"]]
+    table_data = grouped[["Art", "Rank", "Tracks", "💚", "Album", "Release Date", "🔗"]]
 
     return table_data
