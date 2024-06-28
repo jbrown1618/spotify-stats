93e5f106bc55198ce90d6b03f6c895f74e5de322
Josh Brown <jbrown1618@github.com>
2024-06-28 17:03:21 -0400

    Fix column not found error

--- summarize/tables/albums_table.py
+++ summarize/tables/albums_table.py
@@ -4,9 +4,23 @@ from utils.markdown import md_image
 from utils.util import spotify_link, first
 
 def albums_table(tracks: pd.DataFrame):
-    grouped = tracks.groupby("album_uri").agg({"track_uri": "count", "track_liked": "sum", "album_name": first, "album_image_url": first, "album_release_date": first}).reset_index()
+    grouped = tracks.groupby("album_uri").agg({
+        "track_uri": "count", 
+        "track_liked": "sum", 
+        "album_name": first, 
+        "album_image_url": first, 
+        "album_release_date": first, 
+        "album_rank": first
+    }).reset_index()
     grouped = grouped.sort_values(by=["track_uri", "track_liked", "album_name"], ascending=False)
-    grouped = grouped.rename(columns={"track_uri": "Tracks", "track_liked": "💚", "album_name": "Album", "album_release_date": "Release Date", "album_rank": "Rank"})
+    grouped = grouped.rename(columns={
+        "track_uri": "Tracks", 
+        "track_liked": "💚", 
+        "album_name": "Album", 
+        "album_release_date": 
+        "Release Date", 
+        "album_rank": "Rank"
+    })
 
     grouped["Art"] = grouped["album_image_url"].apply(lambda src: md_image("", src, 50))
     grouped["🔗"] = grouped["album_uri"].apply(lambda uri: spotify_link(uri))
