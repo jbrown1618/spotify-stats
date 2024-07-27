c4e40c84837ff59c774a423b37b8f92eedd35951
Josh Brown <jbrown1618@github.com>
2024-07-27 13:12:45 -0400

    Rewrite reduction of tick interval

--- utils/tick_labels.py
+++ utils/tick_labels.py
@@ -4,10 +4,10 @@ def get_ticks(max: int, min: int):
     diff = max - min
     exponent = math.floor(math.log10(diff))
     interval = 10 ** exponent
-    ticks = [min] + [i * interval for i in range(-10, 10) if i * interval > min and i * interval < max] + [max]
-
-    if len(ticks) < 5:
+    
+    if diff < interval * 3:
         interval = int(interval / 2)
-        ticks = [min] + [i * interval for i in range(-20, 20) if i * interval > min and i * interval < max] + [max]
 
-    return ticks
+    middle_ticks = [i * interval for i in range(-10, 10) if i * interval > min and i * interval < max]
+
+    return [min] + middle_ticks + [max]
