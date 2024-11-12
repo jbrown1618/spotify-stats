00df55279abc6e675859d4bc3a6b4824c4cc9e18
Josh Brown <jbrown1618@github.com>
2024-11-11 20:54:23 -0500

    Default to dark theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <MantineProvider>
+      <MantineProvider defaultColorScheme="dark">
         <App />
       </MantineProvider>
     </QueryClientProvider>
