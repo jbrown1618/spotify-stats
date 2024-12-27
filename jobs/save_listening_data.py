e47c0ed56167849683f67a2cb265943afb90456a
Josh Brown <jbrown1618@github.com>
2024-12-27 13:18:35 -0500

    Tweak saving of listening data

--- jobs/save_listening_data.py
+++ jobs/save_listening_data.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 from data.raw import get_connection
 from spotify.spotify_client import get_spotify_client
 
 played_at_date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-max_listening_period_s = 5 * 24 * 60 * 60  # 5 days
+max_listening_period_s = 7 * 24 * 60 * 60  # 7 days
 
 def save_listening_data():
     sp = get_spotify_client()
@@ -18,45 +18,77 @@ def save_listening_data():
         plays_data.append({ "track_uri": track_uri, "time": time })
     plays = pd.DataFrame(plays_data)
 
-    current, min_time = get_listening_period(plays['time'].min(), plays['time'].max())
-    period_id = current[0]
+    current_min, current_id, new_min, new_id = get_listening_period(plays['time'].min(), plays['time'].max())
 
-    plays = plays[plays['time'] >= min_time]
+    current_plays = plays[(plays['time'] >= current_min) & (plays['time'] < new_min)] \
+                    if new_min is not None \
+                    else plays[plays['time'] >= current_min]
+    
+    new_plays = plays[plays['time'] >= new_min] \
+                if new_min is not None \
+                else plays.head(0)
 
-    play_counts = plays.groupby('track_uri')\
+    current_play_counts = current_plays.groupby('track_uri')\
         .agg({"time": "count"})\
         .reset_index()\
         .rename(columns={"time": "stream_count"})
+    
+    update_play_counts(current_id, current_play_counts)
+    
+    if len(new_plays) > 0:
+        new_play_counts = new_plays.groupby('track_uri')\
+            .agg({"time": "count"})\
+            .reset_index()\
+            .rename(columns={"time": "stream_count"})
+        update_play_counts(new_id, new_play_counts)
 
-    update_play_counts(period_id, play_counts)
 
 
 def get_listening_period(min_time: float, max_time: float):
+    """
+    Returns:
+    - The min time to consider for the current period
+    - The ID of the current period
+    - The min time to consider for the new period, if any
+    - The ID of the new period, if any
+    """
+    month = datetime.fromtimestamp(max_time).month
     current = get_latest_listening_period()
 
     if current is None:
         # This case should not be hit often; it accounts for the case where
         # there are no listening periods in the database yet.
         create_listening_period(min_time, max_time)
-        return get_latest_listening_period(), min_time
+        return min_time, get_latest_listening_period()[0], None, None
 
     current_id, current_from, current_to = current
-    if max_time - current_from.timestamp() > max_listening_period_s:
+    if month != current_to.month:
+        # If we are in a new month, extend the current period to the end of
+        # the month and create a new period starting at the beginning of the new month
+        start_of_new_month = datetime.fromtimestamp(max_time).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        end_of_current_month = start_of_new_month - timedelta(seconds=1)
+        
+        update_listening_period(current_id, end_of_current_month.timestamp())
+        create_listening_period(start_of_new_month.timestamp(), max_time)
+        return current_to.timestamp(), current_id, start_of_new_month.timestamp(), get_latest_listening_period()[0]
+    
+    elif max_time - current_from.timestamp() > max_listening_period_s:
         # If updating the current listening period would make it longer
         # than the maximum, create a new listening period that starts
         # just after the current one ends
         new_period_start = current_to.timestamp() + 1
         create_listening_period(new_period_start, max_time)
-        return get_latest_listening_period(), new_period_start
+        return new_period_start, get_latest_listening_period()[0], None, None
+    
     else:
         # Otherwise, update the current listening period to extend to
         # the current time.
         update_listening_period(current_id, max_time)
-        return get_latest_listening_period(), current_to.timestamp() + 1
+        return current_to.timestamp() + 1, get_latest_listening_period()[0], None, None
 
 
 
-def create_listening_period(from_time, to_time):
+def create_listening_period(from_time: float, to_time: float):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
@@ -66,7 +98,7 @@ def create_listening_period(from_time, to_time):
     conn.commit()
 
 
-def update_listening_period(id, to_time):
+def update_listening_period(id, to_time: float):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
@@ -107,3 +139,7 @@ def update_play_counts(period_id: int, play_counts: pd.DataFrame):
             "stream_count": row["stream_count"]
         })
     conn.commit()
+
+
+if __name__ == '__main__':
+    save_listening_data()
