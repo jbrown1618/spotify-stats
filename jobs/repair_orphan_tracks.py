a63dcd2fabb6cc5152c58062466c66790b7e6621
Josh Brown <jbrown1618@github.com>
2024-12-27 14:36:23 -0500

    Repair orphan tracks from listening history

--- jobs/repair_orphan_tracks.py
+++ jobs/repair_orphan_tracks.py
@@ -33,6 +33,14 @@ def get_orphan_tracks():
         INNER JOIN track t ON t.uri = tt.track_uri
         WHERE tt.track_uri NOT IN (
             SELECT pt.track_uri FROM playlist_track pt
+        )
+        UNION ALL (
+            SELECT distinct h.track_uri, t.name
+            FROM listening_history h
+            INNER JOIN track t ON t.uri = h.track_uri
+            WHERE h.track_uri NOT IN (
+                SELECT pt.track_uri FROM playlist_track pt
+            )
         );
     ''')
     return cursor.fetchall()
