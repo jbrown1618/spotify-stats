f8c7e060442f575acd9b8030353128d229f00d0d
Josh Brown <jbrown1618@github.com>
2026-01-10 14:51:54 -0500

    Restore orphan repairs

--- data/sql/queries/repair_orphan_track.sql
+++ data/sql/queries/repair_orphan_track.sql
@@ -1,7 +1,48 @@
--- Update all streams from orphan track to replacement track
+-- Update all streams from orphan track to replacement track in track_stream table
 UPDATE track_stream
 SET track_uri = %(replacement_uri)s
 WHERE track_uri = %(orphan_uri)s;
+
+-- Also update the legacy listening_history table (dual-write during migration)
+UPDATE listening_history h
+SET track_uri = %(replacement_uri)s
+WHERE h.track_uri = %(orphan_uri)s
+AND NOT EXISTS (
+    SELECT track_uri
+    FROM listening_history
+    WHERE track_uri = %(replacement_uri)s
+    AND listening_period_id = h.listening_period_id
+);
+
+-- If the listening period has both the orphan and replacement
+-- version of the track, add the orphan totals to the
+-- replacement version.      
+UPDATE listening_history h
+SET stream_count = h.stream_count + (
+    SELECT stream_count
+    FROM listening_history
+    WHERE track_uri = %(orphan_uri)s
+    AND listening_period_id = h.listening_period_id
+)
+WHERE h.track_uri = %(replacement_uri)s
+AND EXISTS (
+    SELECT track_uri
+    FROM listening_history
+    WHERE track_uri = %(orphan_uri)s
+    AND listening_period_id = h.listening_period_id
+);
+
+-- If the listening period has both the orphan and replacement
+-- version of the track, we have merged them above so we can
+-- now delete the orphan version.
+DELETE FROM listening_history h
+WHERE track_uri = %(orphan_uri)s
+AND EXISTS (
+    SELECT track_uri
+    FROM listening_history
+    WHERE track_uri = %(replacement_uri)s
+    AND listening_period_id = h.listening_period_id   
+);
 
 UPDATE sp_track_mb_recording r
 SET spotify_track_uri = %(replacement_uri)s
