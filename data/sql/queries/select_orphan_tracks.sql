7d352ef258e482ad61a303bb5649acad8bd242de
Josh Brown <jbrown1618@github.com>
2026-01-11 14:20:26 -0500

    Improve query perf

--- data/sql/queries/select_orphan_tracks.sql
+++ data/sql/queries/select_orphan_tracks.sql
@@ -1,18 +1,12 @@
-SELECT DISTINCT track_uri, name
-FROM (
-    -- Orphans from track_stream
-    SELECT s.track_uri, t.name
-    FROM track_stream s
-    INNER JOIN track t ON t.uri = s.track_uri
-    WHERE s.track_uri NOT IN (
-        SELECT pt.track_uri FROM playlist_track pt
-    )
+-- Find tracks that have streams but are not in any playlist
+-- Uses UNION to dedupe track URIs from both tables first, then checks against playlists
+WITH streamed_tracks AS (
+    SELECT DISTINCT track_uri FROM track_stream
     UNION
-    -- Orphans from listening_history
-    SELECT h.track_uri, t.name
-    FROM listening_history h
-    INNER JOIN track t ON t.uri = h.track_uri
-    WHERE h.track_uri NOT IN (
-        SELECT pt.track_uri FROM playlist_track pt
-    )
-) AS orphans
+    SELECT DISTINCT track_uri FROM listening_history
+)
+SELECT st.track_uri, t.name
+FROM streamed_tracks st
+INNER JOIN track t ON t.uri = st.track_uri
+LEFT JOIN playlist_track pt ON pt.track_uri = st.track_uri
+WHERE pt.track_uri IS NULL
